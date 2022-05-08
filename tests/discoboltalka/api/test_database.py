41bb9921cba93459b1552b438b6175563a0486e4
lev <levbezrukov@bk.ru>
2022-05-08 23:48:53 +0200

    v 0.2.0
    Fix tests

--- tests/discoboltalka/api/test_database.py
+++ tests/discoboltalka/api/test_database.py
@@ -4,8 +4,8 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import select, insert
 
-from discoboltalka.api.database import (
-    metadata,
+from discoboltalka.api.adapters.orm import sqlalchemy_metadata
+from discoboltalka.api.repositories.dialog_repository import (
     context_table,
     dialog_table,
     DialogRepository,
@@ -19,8 +19,8 @@ class TestDialogRepository(IsolatedAsyncioTestCase):
         )
 
         async with engine.begin() as connect:
-            await connect.run_sync(metadata.drop_all)
-            await connect.run_sync(metadata.create_all)
+            await connect.run_sync(sqlalchemy_metadata.drop_all)
+            await connect.run_sync(sqlalchemy_metadata.create_all)
 
         self._session: AsyncSession = sessionmaker(bind=engine, class_=AsyncSession)()
         self._repository = DialogRepository(session=self._session, max_len=5)
@@ -38,7 +38,7 @@ class TestDialogRepository(IsolatedAsyncioTestCase):
     async def test_get_last_contexts_without_context(self) -> None:
         user_id = 501089151089770517
 
-        await self._session.execute(insert(dialog_table).values(user_id=user_id))
+        await self._session.execute(insert(dialog_table).values(id=user_id))
 
         result = await self._repository.get_last_contexts(user_id)
         self.assertEqual(result, [])
@@ -46,23 +46,20 @@ class TestDialogRepository(IsolatedAsyncioTestCase):
     async def test_get_last_contexts_with_context(self) -> None:
         user_id = 501089151089770517
 
-        dialog_insert_result = await self._session.execute(
-            insert(dialog_table).values(user_id=user_id).returning(dialog_table.c.id),
-        )
-        dialog_id = dialog_insert_result.fetchone().id
+        await self._session.execute(insert(dialog_table).values(id=user_id))
 
         await self._session.execute(
             insert(context_table).values(
                 user_request="Test11",
                 bot_response="Test12",
-                dialog_id=dialog_id,
+                dialog_id=user_id,
             ),
         )
         await self._session.execute(
             insert(context_table).values(
                 user_request="Test13",
                 bot_response="Test14",
-                dialog_id=dialog_id,
+                dialog_id=user_id,
             ),
         )
 
@@ -94,17 +91,15 @@ class TestDialogRepository(IsolatedAsyncioTestCase):
                     id=1,
                     user_request="Test1",
                     bot_response="Test2",
-                    dialog_id=1,
-                    id_1=1,
-                    user_id=user_id,
+                    dialog_id=user_id,
+                    id_1=user_id,
                 ),
                 dict(
                     id=2,
                     user_request="Test3",
                     bot_response="Test4",
-                    dialog_id=1,
-                    id_1=1,
-                    user_id=user_id,
+                    dialog_id=user_id,
+                    id_1=user_id,
                 ),
             ]
         )
